7ad4781a32ce850c3357224dfe9917e86f9dbf9f
Purnendu <mailtopv2299@gmail.com>
2021-05-04 21:32:35 +0530

    Replaced cards with containers

--- lib/main.dart
+++ lib/main.dart
@@ -14,83 +14,79 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-              children: [
-                CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('images/dp.jfif'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/dp.jfif'),
+              ),
+              Text(
+                'Purnendu Vashistha',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
-                Text(
-                  'Purnendu Vashistha',
-                  style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  letterSpacing: 2.25
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.00, horizontal: 25.00),
+                //color: Colors.white,  Card comes with default white color
+                  child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
                   ),
-                ),
-                Text(
-                  'FLUTTER DEVELOPER',
-                  style: TextStyle(
-                      color: Colors.teal.shade100,
+                  title: Text(
+                    '+91 9472101347',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
                       fontFamily: 'Source Sans Pro',
                       fontSize: 20.0,
-                      letterSpacing: 2.25
+                    ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10.00, horizontal: 25.00),
-                  color: Colors.white,
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.phone,
-                        color: Colors.teal,
+                )
+              ),
+              Card(
+                //color: Colors.white,  Card comes with default white color
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'mailtopv2299@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
                       ),
-                      SizedBox(
-                        width: 10.00,
-                      ),
-                      Text(
-                        '+91 9472101347',
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.email,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10.00,
-                      ),
-                      Text(
-                        'mailtopv2299@gmail.com',
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20.00,
-                        ),
-                      )
-                    ],
-                  ),
-
-                ),
-              ],
-            )
+                    ),
+                  )
+              ),
+            ],
+          )
         ),
       ),
     );
   }
 }
+
